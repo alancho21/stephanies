2187303f8efbd6dda98069ef3628d829f04d46a7
alancho21 <alansamp2004@gmail.com>
2024-03-19 23:54:53 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes to be committed:
    	new file:   resources/cssStyles/menuAdmin.css
    	modified:   resources/views/MenuAdmin.blade.php

---
+++ resources/views/MenuAdmin.blade.php
@@ -0,0 +1,49 @@
+@extends('app')
+
+@section('title', 'Menú Administrador')
+
+@section('extra-css')
+    {{-- Si tienes estilos específicos para esta vista, enlázalos aquí --}}
+    <link href="{{ asset('cssStyles/menuAdmin.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+<body style="background: url('{{ asset('images/v152_180.png') }}') no-repeat center center fixed; background-size: cover;">
+
+    
+<section class="container">
+        <div class="header">Bienvenido Admin</div>
+        <div class="card-container">
+            <div class="card" onclick="location.href='{{ route('gestionarmenu') }}'">
+                <img src="images\v225_131.png" class="icon" alt="Gestionar Menú">
+                <div class="text">Gestionar Menú</div>
+            </div>
+            <div class="card" onclick="location.href='{{ route('gestionarusuarios') }}'">
+                <img src="images\v225_130.png" class="icon" alt="Gestionar Usuarios">
+                <div class="text">Gestionar Usuarios</div>
+            </div>
+            <div class="card" onclick="location.href='{{ route('inventario') }}'">
+                <img src="images\v225_129.png" class="icon" alt="Inventario">
+                <div class="text">Inventario</div>
+            </div>
+            <div class="card" onclick="location.href='{{ route('pedidostock') }}'">
+                <img src="images\v160_223.png" class="icon" alt="Pedido de Stock">
+                <div class="text">Pedido de Stock</div>
+            </div>
+            <div class="card" onclick="location.href='{{ route('comentarios') }}'">
+                <img src="images\v160_225.png" class="icon" alt="Comentarios">
+                <div class="text">Comentarios</div>
+            </div>
+            <div class="card" onclick="location.href='{{ route('gestionarproveedores') }}'">
+                <img src="images\shop-solid.svg" class="icon" alt="Gestionar Proveedores">
+                <div class="text">Gestionar Proveedores</div>
+            </div>
+            <div class="card" onclick="location.href='{{ route('pedidos') }}'">
+                <img src="images\store-solid.svg" class="icon" alt="pedidos">
+                <div class="text">Pedidos</div>
+            </div>
+        </div>
+    
+</section>
+</body>
+@endsection
