6ee3ab9de770be169d6477ad3dfdab2f98c10b74
alancho21 <alansamp2004@gmail.com>
2024-03-24 20:57:44 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   app/Http/Controllers/ProductController.php
    	new file:   app/Http/Requests/ProductRequest.php
    	modified:   app/Models/Product.php
    	modified:   package-lock.json
    	new file:   resources/views/product/create.blade.php
    	new file:   resources/views/product/edit.blade.php
    	new file:   resources/views/product/form.blade.php
    	new file:   resources/views/product/index.blade.php
    	new file:   resources/views/product/show.blade.php
    	modified:   routes/web.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,22 +2,54 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Product
+ *
+ * @property $id
+ * @property $name
+ * @property $price
+ * @property $description
+ * @property $category_id
+ * @property $created_at
+ * @property $updated_at
+ * @property $ruta_imagen
+ *
+ * @property Category $category
+ * @property Stock[] $stocks
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Product extends Model
 {
-    use HasFactory;
+    
 
-    public function category(){
-        return $this->belongsTo(Category::class);
-    }
+    protected $perPage = 20;
 
-    public function images(){
-        return $this->morphMany(Image::class, 'imageable');
-    }
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'price', 'description', 'category_id', 'ruta_imagen'];
 
-    public function stock(){
-        return $this->hasMany(Stock::class);
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(\App\Models\Category::class, 'category_id', 'id');
     }
+    
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function stocks()
+    {
+        return $this->hasMany(\App\Models\Stock::class, 'id', 'product_id');
+    }
+    
+
 }
