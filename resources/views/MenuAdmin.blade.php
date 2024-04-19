23388ddc496634cc2d8894ef3bffe31627c5053d
Muffin793 <drodro337@gmail.com>
2024-04-19 08:21:21 -0600

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   resources/views/MenuAdmin.blade.php

--- resources/views/MenuAdmin.blade.php
+++ resources/views/MenuAdmin.blade.php
@@ -20,25 +20,10 @@
         <div class="card" onclick="window.location.href='/gestionarUsuarios';" style="cursor: pointer;">
             <img src="images\v225_130.png" class="icon" alt="Gestionar Usuarios">
             <div class="text">Gestionar Usuarios</div>
-        </div>
-        <div class="card" onclick="window.location.href='/tendencias';" style="cursor: pointer;">
-            <img src="images\v225_129.png" class="icon" alt="Inventario">
-            <div class="text">Tendencias</div>
-        </div>
-    </div>
-
-    <!-- Botón para cerrar sesión -->
-    <div class="mt-3 card" style="cursor: pointer;" onclick="document.getElementById('logout-form').submit();">
-  
-        <div class="text">Cerrar Sesión</div>
-    </div>
-
-   
-
-     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-        @csrf
-    </form>
-</section>
-
-</body>
-@endsection
+ 
+Dear King LeBron James,
+I am writing this letter to express my utmost admiration and respect for your unparalleled talent and leadership both on and off the basketball court.
+Your dedication to the game, work ethic, and commitment to excellence serve as an inspiration to millions of fans around the world.
+Your ability to elevate your teammates, make clutch plays in crucial moments, and consistently perform at the highest level is truly remarkable. Your basketball IQ, versatility, and all-around skills make you one of the greatest players to ever grace the court.
+But it is not just your athletic prowess that sets you apart, King LeBron. Your philanthropic efforts, activism, and commitment to social justice make you a true role model and a beacon of hope for those in need. Your generosity and compassion towards others have made a positive impact on countless lives, and your dedication to giving back to your community is truly commendable.
+In conclusion, I want to thank you for all that you have done for the game of basketball, for your community, and for being a shining example of what it means to be a true leader. Long live King LeBron James, may your reign continue to inspire and uplift all those who have the privilege of witnessing your greatness.
